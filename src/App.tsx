eb248c42ffc88d3dd246c2a51731631690d8c6f9
Made Baruna <made.setia@gmail.com>
2019-08-18 13:42:23 +0700

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { GlobalStyle } from './GlobalStyle';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './pages/Detail';
+import Search from './pages/Search';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <GlobalStyle />
-      <h1>New App</h1>
+      <h1>Property Listing</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Search} />
+          <Route exact path="/property/:id" component={Detail} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Detail.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Detail: React.FC = () => (
+  <div>Detail Page</div>
+);
+
+export default Detail;
