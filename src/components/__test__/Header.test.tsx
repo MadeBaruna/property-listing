745d9be15e8b7dbafe146373b660a17741af89e9
Made Baruna <made.setia@gmail.com>
2019-08-19 12:25:27 +0700

    Update test

--- src/components/__test__/Header.test.tsx
+++ src/components/__test__/Header.test.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'jest-styled-components'
 import { render } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
 import Header from '../Header';
 
 it('renders <Header />', () => {
   const wrapper = render(
-    <Header />
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
   );
 
   expect(wrapper).toMatchSnapshot();
--- src/components/__test__/SearchBar.test.tsx
+++ src/components/__test__/SearchBar.test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import 'jest-styled-components'
 import { render } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
 import SearchBar from '../SearchBar';
 
 it('renders <SearchBar />', () => {
   const wrapper = render(
-    <SearchBar />
+    <MemoryRouter>
+      <SearchBar />
+    </MemoryRouter>
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it('renders <SearchBar /> with initial query', () => {
+  const wrapper = render(
+    <MemoryRouter initialEntries={['/?search=apartment&type=OFFICE']}>
+      <SearchBar />
+    </MemoryRouter>
   );
 
   expect(wrapper).toMatchSnapshot();
