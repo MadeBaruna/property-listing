1496b0dd60c77a7463ee7f726fd7bf9a5589bf00
Made Baruna <made.setia@gmail.com>
2019-08-19 11:33:33 +0700

    Update config

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'property-listing-backend',
-      url: 'http://172.29.15.49:5001/graphql',
+      url: 'http://localhost:5001/graphql',
     },
   },
 };
